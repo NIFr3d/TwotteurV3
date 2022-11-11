edf4a35d31842b5a0f98753c3ceb102c4041a24c
NIFr3d <fred-dls@orange.fr>
2022-11-11 14:28:09 +0100

    fin transition thymeleaf

--- src/main/resources/static/js/twotpreview.js
+++ src/main/resources/static/js/twotpreview.js
@@ -49,3 +49,40 @@ function afficher_msg(id) {
 function fadeOut(){
     $('.answerForm, #overlay-back').fadeOut(500);
 }
+function sqlToJsDate(sqlDate){
+    //sqlDate in SQL DATETIME format ("yyyy-mm-dd hh:mm:ss.ms")
+    var sqlDateArr1 = sqlDate.split("-");
+    //format of sqlDateArr1[] = ['yyyy','mm','dd hh:mm:ms']
+    var sYear = sqlDateArr1[0];
+    var sMonth = (Number(sqlDateArr1[1]) - 1).toString();
+    var sqlDateArr2 = sqlDateArr1[2].split(" ");
+    //format of sqlDateArr2[] = ['dd', 'hh:mm:ss.ms']
+    var sDay = sqlDateArr2[0];
+    var sqlDateArr3 = sqlDateArr2[1].split(":");
+    //format of sqlDateArr3[] = ['hh','mm','ss.ms']
+    var sHour = sqlDateArr3[0];
+    var sMinute = sqlDateArr3[1];
+    var sqlDateArr4 = sqlDateArr3[2].split(".");
+    //format of sqlDateArr4[] = ['ss','ms']
+    var sSecond = sqlDateArr4[0];
+    var sMillisecond = sqlDateArr4[1];
+
+    return new Date(sYear,parseInt(sMonth),sDay,sHour,sMinute,sSecond,sMillisecond);
+}
+function initdate(id,javadate){
+    var jsdate=sqlToJsDate(javadate);
+    var now=new Date();
+    var timedif=now.getTime()-jsdate.getTime();
+    if(timedif>86400000){
+        if(jsdate.getUTCDate()<10) affichage=jsdate.getMonth()+"/0"+jsdate.getUTCDate();
+        else affichage=jsdate.getMonth()+"/"+jsdate.getUTCDate();
+    }else{
+        if(timedif<3600000){
+            affichage=Math.round(Math.abs(timedif/(1000*60))%60)+"min";
+        }else{
+            if(now.getDate()==jsdate.getDate()) affichage=jsdate.getHours()+":"+jsdate.getMinutes();
+            else affichage="Hier";
+        }
+    }
+    document.getElementById("date"+id).innerHTML=affichage;
+}
